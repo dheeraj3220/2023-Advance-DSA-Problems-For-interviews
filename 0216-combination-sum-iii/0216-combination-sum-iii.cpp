79097bbeac2bea5784b3d647925697cbd64a53b2
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-01-24 11:32:50 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (48.47%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void findComb(int k,int n,vector<vector<int>>&res,vector<int>temp,int index){
+        if(k==0){
+            if(n==0) {
+                res.push_back(temp);
+            }
+            return ;
+        }
+        for(int i=index;i<=9;i++){
+            if(n-i<0) break;
+            temp.push_back(i);
+            findComb(k-1,n-i,res,temp,i+1);
+            temp.pop_back();
+        }
+        
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        findComb(k,n,res,temp,1);
+        return res;
+    }
+};
