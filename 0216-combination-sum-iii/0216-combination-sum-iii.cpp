e59406c458c9ea8e1c91802a45b7355ea74e55e7
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-01-24 11:34:38 +0530

    Time: 10 ms (8.83%), Space: 6.6 MB (43.46%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -13,7 +13,6 @@ public:
             findComb(k-1,n-i,res,temp,i+1);
             temp.pop_back();
         }
-        
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> res;
