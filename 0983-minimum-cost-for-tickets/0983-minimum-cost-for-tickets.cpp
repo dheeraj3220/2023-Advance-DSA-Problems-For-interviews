871a36f2d09abb5431391d0902ac3e36b965ddc1
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-03-28 23:36:52 +0530

    Time: 24 ms (5.02%), Space: 60.1 MB (5.22%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int helper(vector<int>& days, vector<int>& costs,int curIndex,int passDay,vector<vector<int>>&dp){
+        if(curIndex>=days.size()) return 0;
+        int minAns=INT_MAX;
+        if(dp[curIndex][passDay+1]!=-1) return dp[curIndex][passDay+1];
+        if(days[curIndex]<passDay){
+            minAns=helper(days,costs,curIndex+1,passDay,dp);
+        }
+        else{
+            int A=costs[0]+helper(days,costs,curIndex+1,days[curIndex]+1,dp);
+            int B=costs[1]+helper(days,costs,curIndex+1,days[curIndex]+7,dp);
+            int C=costs[2]+helper(days,costs,curIndex+1,days[curIndex]+30,dp);
+            minAns=min(A,min(B,C));
+        }
+        return dp[curIndex][passDay+1]=minAns;
+        
+        
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<vector<int>> dp(365,vector<int>(410,-1));
+        return helper(days,costs,0,-1,dp);
+    }
+};
