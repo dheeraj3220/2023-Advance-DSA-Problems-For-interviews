d70909678513d937e02bebc87e4219e50ae879d3
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-04-03 00:48:41 +0530

    Time: 246 ms (70.86%), Space: 99.8 MB (63.36%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -4,8 +4,7 @@ public:
         sort(p.begin(),p.end());
         vector<int> res;
         for(int i=0;i<s.size();i++){
-            long long ans = long(s[i]) * p[p.size()-1];
-           if(ans<suc) {
+           if(long(s[i]) * p[p.size()-1]<suc) {
                res.push_back(0);
                continue;
            }
@@ -13,8 +12,7 @@ public:
            int high=p.size()-1;
             while(low<high){
                 int mid=(low+high)/2;
-                long long ans =long(s[i])*p[mid];
-                if(ans>=suc) high=mid;
+                if(long(s[i])*p[mid]>=suc) high=mid;
                 else low=mid+1;
             }
             res.push_back(p.size()-low);
