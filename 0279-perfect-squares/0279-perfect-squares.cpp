619c57b7cf2f6d6a820bc9ea0747d5a5465ed012
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-05-18 20:56:40 +0530

    Time: 318 ms (25.66%), Space: 9.9 MB (28.54%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int helper(int n,int curNum,int curSum,vector<vector<int>>&dp){
+    int helper(int n,int curNum,int curSum,vector<int>&dp){
         if(curSum>n) return 1e9;
         if(curSum==n) return 0;
-        if(dp[curNum][curSum]!=-1) return dp[curNum][curSum];
+        if(dp[curSum]!=-1) return dp[curSum];
         int minNum=1e8;
-        for(int i=curNum;i>=1;i--){
-            if(curSum+pow(i,2)<=n) minNum=min(minNum,1+helper(n,i,curSum+pow(i,2),dp));
+        for(int i=1;i<=sqrt(n);i++){
+            minNum=min(minNum,1+helper(n,i,curSum+pow(i,2),dp));
         }
-        return dp[curNum][curSum]=minNum;
+        return dp[curSum]=minNum;
     }
     int numSquares(int n) {
-        vector<vector<int>> dp(sqrt(n)+1,vector<int>(n+1,-1));
+        vector<int> dp(n+1,-1);
         return helper(n,sqrt(n),0,dp);
     }
 };
