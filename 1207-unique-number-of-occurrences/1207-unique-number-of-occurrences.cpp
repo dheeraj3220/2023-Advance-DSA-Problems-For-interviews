5096d52382f4108fbdae2fd572f1a3a79ce13624
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-06-27 23:16:16 +0530

    Time: 10 ms (6.58%), Space: 8.6 MB (7.38%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,10 +3,8 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         vector<int> acc(2001,0);
         for(auto a: arr) acc[a+1000]++;
-        set<int> mset;
-        for(auto a:acc) 
-            if(mset.count(a)==1 && a!=0) return false;
-            else mset.insert(a);
+        sort(acc.begin(),acc.end());
+        for(int i=0;i<acc.size()-1;i++) if(acc[i]==acc[i+1] && acc[i]!=0) return false;
         return true;
     }
 };
