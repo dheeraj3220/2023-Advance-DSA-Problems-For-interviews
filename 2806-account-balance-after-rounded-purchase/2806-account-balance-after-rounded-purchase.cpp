9a712614d0602ce331cc458ec653f7f54176b044
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-08-08 16:38:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (61.41%) - LeetHub

--- 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.cpp
+++ 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-    int accountBalanceAfterPurchase(int pa) {
-        return pa%10<5?100-(pa/10)*10:100-((pa/10)+1)*10;
+    int accountBalanceAfterPurchase(int x) {
+        int rem;
+    int ans=x/10;
+    int finalans;
+    rem=x%10;
+    if(rem<5){
+       ans=ans*10; 
+    }else{
+        ans=(ans+1)*10;
+    }
+    
+    finalans=100-ans;
+    return finalans;
     }
 };
