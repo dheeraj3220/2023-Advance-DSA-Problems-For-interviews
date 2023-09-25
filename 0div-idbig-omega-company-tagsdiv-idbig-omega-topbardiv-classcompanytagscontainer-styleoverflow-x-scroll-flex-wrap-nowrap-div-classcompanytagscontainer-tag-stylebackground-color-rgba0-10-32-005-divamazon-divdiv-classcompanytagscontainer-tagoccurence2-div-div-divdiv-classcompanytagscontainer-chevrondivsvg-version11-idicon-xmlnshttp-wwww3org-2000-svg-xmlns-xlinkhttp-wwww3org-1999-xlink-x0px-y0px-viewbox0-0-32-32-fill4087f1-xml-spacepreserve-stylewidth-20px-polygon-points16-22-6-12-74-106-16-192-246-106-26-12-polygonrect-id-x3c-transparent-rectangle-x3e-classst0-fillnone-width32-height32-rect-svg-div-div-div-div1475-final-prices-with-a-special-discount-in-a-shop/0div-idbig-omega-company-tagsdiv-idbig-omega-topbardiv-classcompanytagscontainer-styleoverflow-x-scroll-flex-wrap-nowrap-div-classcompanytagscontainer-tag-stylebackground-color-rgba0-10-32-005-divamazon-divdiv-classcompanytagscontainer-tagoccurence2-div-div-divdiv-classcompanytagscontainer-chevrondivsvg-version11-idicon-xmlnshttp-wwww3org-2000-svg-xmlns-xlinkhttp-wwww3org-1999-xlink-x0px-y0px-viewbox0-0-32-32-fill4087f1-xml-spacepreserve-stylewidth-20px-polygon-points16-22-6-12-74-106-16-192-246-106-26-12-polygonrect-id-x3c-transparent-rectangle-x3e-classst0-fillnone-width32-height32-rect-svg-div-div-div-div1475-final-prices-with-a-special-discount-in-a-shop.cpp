f11ec0b335e5204586aa6fbaaca1956b29932f60
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2023-09-26 00:23:34 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (5.76%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1475-final-prices-with-a-special-discount-in-a-shop/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack <int> stk;
+        vector<int> res(prices.size(),0);
+        for(int i=prices.size()-1;i>=0;i--){
+            while(stk.size() && stk.top()>prices[i]) stk.pop();
+            if(stk.size()) res[i]=prices[i]-stk.top();
+            else res[i]=prices[i];
+            stk.push(prices[i]);
+        }
+        return res;
+    }
+};
